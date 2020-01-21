2383e6505697c8dea39e232899a6f894fe9352e3
nirvik00 <nirviksaha@gatech.edu>
2020-01-20 21:06:03 -0500

    new bsp perfect algorithm complete

---
+++ repos/gui_Installer/gui_Installer/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace gui_Installer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(134, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(95, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(284, 58);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(91, 325);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(288, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Installer for plug-in for Rhino";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(108, 344);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(252, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "created at Perkins and Will";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 486);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ repos/gui_Installer/gui_Installer/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gui_Installer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Hello " + Environment.UserName;
+            button1.Text = "Install Rhino Plug-in";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string currentDir = Directory.GetCurrentDirectory();
+                string path = Directory.GetCurrentDirectory();
+                string installPath = Directory.GetDirectories(path)[0];//install directory
+                path = System.IO.Path.Combine(path, installPath);
+                Console.WriteLine("dir1 :" + path);
+
+                string guidPath = Directory.GetDirectories(path)[0];//guid directory
+                path = System.IO.Path.Combine(path, guidPath);
+                Console.WriteLine("dir2 :", path);
+
+                string devPath = Directory.GetDirectories(path)[0];//dev directory
+                path = System.IO.Path.Combine(path, devPath);
+                string guidName = Path.GetFileName(Path.GetDirectoryName(path));
+
+                string[] files = Directory.GetFiles(path);
+                string devName = new DirectoryInfo(path).Name;
+                string reqPath = Path.Combine(guidName, devName);
+
+                string destinationDir = @"C:\Users\" + Environment.UserName + @"\AppData\Roaming\McNeel\Rhinoceros\5.0\Plug-ins\PythonPlugins";
+
+                destinationDir = Path.Combine(destinationDir, reqPath);
+                if (!Directory.Exists(destinationDir))
+                {
+                    System.IO.Directory.CreateDirectory(destinationDir);
+                }
+                foreach (string s in files)
+                {
+                    string fileName = System.IO.Path.GetFileName(s);
+                    string destFile = System.IO.Path.Combine(destinationDir, fileName);
+                    System.IO.File.Copy(s, destFile, true);
+                }
+                string destinationDir2 = @"C:\Users\" + Environment.UserName + @"\AppData\Roaming\McNeel\Rhinoceros\6.0\Plug-ins\PythonPlugins";
+
+                destinationDir2 = Path.Combine(destinationDir2, reqPath);
+                if (!Directory.Exists(destinationDir2))
+                {
+                    System.IO.Directory.CreateDirectory(destinationDir2);
+                }
+                foreach (string s in files)
+                {
+                    string fileName = System.IO.Path.GetFileName(s);
+                    string destFile = System.IO.Path.Combine(destinationDir2, fileName);
+                    System.IO.File.Copy(s, destFile, true);
+                }
+                MessageBox.Show("Installation Succeeded");
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Installation Failed. check admin rights...");
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
